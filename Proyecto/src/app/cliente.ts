d44ea8d6b69cb72300a73a507a2c89ae4746d1ca
Andres Chapeton <andres.chapeton0206@gmail.com>
2020-08-30 23:14:44 -0600

    Funcion descuento

--- Proyecto/src/app/cliente.ts
+++ Proyecto/src/app/cliente.ts
@@ -7,6 +7,19 @@ export class Cliente {
     public tratamiento: string,
     public medicamentos: string,
     public costo: number,
-    public visitas: number
+    public visitas: number,
+    public descuento: number
   ) {}
+
+  descuentos = () => {
+    if (this.visitas == 2) {
+      this.descuento = this.costo * 0.95;
+      return this.descuento;
+    } else if (this.visitas == 4) {
+      this.descuento = this.costo * 0.9;
+      return this.descuento;
+    } else {
+      return this.descuento;
+    }
+  };
 }
--- Proyecto/src/app/cliente/cliente.component.ts
+++ Proyecto/src/app/cliente/cliente.component.ts
@@ -7,9 +7,10 @@ import { Cliente } from '../cliente';
   styleUrls: ['./cliente.component.css']
 })
 export class ClienteComponent implements OnInit {
-  Cliente = new Cliente('', '', '', '', '', '',0, 0);
+  Cliente = new Cliente('', '', '', '', '', '', 0, 0, 0);
   registrar = false;
   visitas = 0;
+  costo = 0;
   constructor() {}
 
   ngOnInit(): void {}
@@ -17,6 +18,11 @@ export class ClienteComponent implements OnInit {
   onSubmit() {
     this.visitas++;
     this.registrar = true;
+
+    // if (Cliente.visitas == 2) {
+    //   Cliente.costo = this.costo * 0.95;
+    // } else if (this.visitas == 4) {
+    //   this.costo = this.costo * 0.8;
+    // // }
   }
-  
 }
